340767ab9f61653e9c3b8f9590830439c6e3438b
YE野落LUO <75648625+YELUO1128@users.noreply.github.com>
2021-09-03 12:13:42 +0800

    Update User.java

--- blog/src/main/java/com/dodo/mblog/entity/User.java
+++ blog/src/main/java/com/dodo/mblog/entity/User.java
@@ -6,11 +6,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-/**
- * @Auther: Administrator
- * @Date: 2018/5/27 09:53
- * @Description:
- */
+
 
 
 public class User {
