ecd861da0ce96a7fe2f0151df501f74e144171cb
YE野落LUO <75648625+YELUO1128@users.noreply.github.com>
2021-09-03 12:14:41 +0800

    Update MessageMapper.java

--- blog/src/main/java/com/dodo/mblog/mapper/MessageMapper.java
+++ blog/src/main/java/com/dodo/mblog/mapper/MessageMapper.java
@@ -4,11 +4,7 @@ import com.dodo.mblog.entity.Message;
 
 import java.util.List;
 
-/**
- * @Auther: Administrator
- * @Date: 2018/5/29 13:11
- * @Description:
- */
+
 public interface MessageMapper {
 
     public Message findMsgById(Integer msgid);
